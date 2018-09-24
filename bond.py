972404ca76cc3f3214d40148a053d66a8c80ae4b
bi4ok <bi4ok23@gmail.com>
2018-09-24 13:08:54 +0300

    4

--- bond.py
+++ bond.py
@@ -1,12 +1,37 @@
-s='Это обычная строка, а в ней адрес почты golos@mail.ru'
-words=s.split(" ")
-print(words)
-while(1==1):
-    if('@mail.ru' in s):
-        print("есть совпадение")
-        for x in s:
-            if(x in '@mail.r'):
-                print(str(x))
-            elif(x in 'u'):
-                print(str(x))
-        break
+import ctypes
+
+class DynArray:
+
+    def __init__(self):
+        self.count = 0
+        self.capacity = 16
+        self.array = self.make_array(self.capacity)
+
+    def __len__(self):
+        return self.count
+
+    def make_array(self, new_capacity):
+        return (new_capacity * ctypes.py_object)()
+
+    def __getitem__(self,i):
+        if i < 0 or i >= self.count:
+            raise IndexError('Index is out of bounds')
+        return self.array[i]
+
+    def resize(self, new_capacity):
+        new_array = self.make_array(new_capacity)
+        for i in range(self.count):
+            new_array[i] = self.array[i]
+        self.array = new_array
+        self.capacity = new_capacity
+
+    def append(self,item):
+        if self.count == self.capacity:
+            self.resize(2*self.capacity)
+        self.array[self.count] = item
+        self.count += 1
+
+da = DynArray()
+for i in range(64):
+    da.append(i)
+    print (da[i])
