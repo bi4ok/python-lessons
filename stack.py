b1f4155d027cbc1740f93eda7ba89aa080f4fd6d
bi4ok <bi4ok23@gmail.com>
2018-10-06 05:23:53 +0300

    исправлен 5.4 баланс

--- stack.py
+++ stack.py
@@ -18,21 +18,19 @@ class Stack:
     def size(self):
         return len(self.stack)
 
-    def balans(self,s1):
+    def balans(self, s1):
         s2 = Stack()
-        for j in range (len(s1)):
-            s2.push(0)    
-        for i in range (len(s1)):
+        for i in range(len(s1)):
             if s1[i] == "(":
                 s2.push("(")
-            elif s1[i] == ")":
+            elif s1[i] == ")" and s2.peak() == "(":
                 s2.pop()
-        if s2.size() == len(s1):
+        if s2.size() == 0:
             print("balans")
         else:
             print("ne balans")
 
-    def s4et(self,vir):
+    def s4et(self, vir):
         s = vir.split()
         s1 = Stack()
         s2 = Stack()
@@ -47,13 +45,17 @@ class Stack:
                     s2.push(s2.pop() * s2.pop())
                 s1.pop()
             elif s[i] == "=":
-                print("Sum = ",s2.pop())
+                print("Sum = ", s2.pop())
             else:
                 s2.push(int(s1.pop()))
         
             
 stack = Stack()
-s = "(())))(((((((())))))"
-s1 = "8 2 + 5 * ="
-stack.s4et(s1)
-stack.balans(s)
+x = "(())()"
+st = "8 2 + 5 * ="
+stack.s4et(st)
+stack.balans(x)
+x = "555"
+st = "123"
+s3 = int(x) + 123
+print()
