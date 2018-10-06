80bd64239a4693daa88632a46f87d04c3874674e
bi4ok <bi4ok23@gmail.com>
2018-10-06 22:33:10 +0300

    6.3, очередь через 2 стэка

--- 6/Queue.py
+++ 6/Queue.py
@@ -3,33 +3,26 @@ import modstack
 
 class Queue:
     def __init__(self):
-        self.items = []
-
-    def enqueue(self, item):
-        self.items.insert(0, item)
-
-    def dequeue(self):
-        return self.items.pop()
+        self.stack_a = modstack.Stack()
+        self.stack_b = modstack.Stack()
 
     def size(self):
-        return len(self.items)
+        return self.stack_a.size()+self.stack_b.size()
 
-    def circle(self, n):
-        for i in range(n):
-            self.enqueue(self.dequeue())
+    def enqueue(self, item):
+        return self.stack_a.push(item)
+
+    def dequeue(self):
+        if self.stack_a.size() > 0:
+            while self.stack_a.size() > 0:
+                self.stack_b.push(self.stack_a.pop())
+        return self.stack_b.pop()
 
 
-qu = Queue()
-qu.enqueue(1)
-qu.enqueue(2)
-qu.enqueue(3)
-qu.circle(5)
-while qu.size() > 0:
-    print(qu.dequeue())
-print()
-s1 = modstack.Stack()
-s2 = modstack.Stack()
-s1.push(5)
-s1.push(4)
-s1.push(3)
-s2.chered(s1, 3)
+s1 = Queue()
+s1.enqueue(1)
+s1.enqueue(2)
+s1.enqueue(3)
+
+while s1.size() > 0:
+    print(s1.dequeue())
