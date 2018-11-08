4227bf5472ea76ee2db38439cf0d1610280128d7
bi4ok <bi4ok23@gmail.com>
2018-11-08 12:23:45 +0300

    rArrayTree+check

---
+++ rArrayTree/arraytree.py
@@ -0,0 +1,112 @@
+import unittest
+
+
+class Node:
+    def __init__(self, val):
+        self.value = val
+        self.parent = None
+        self.r = None
+        self.l = None
+        self.level = 0
+
+
+class ArrayTree:
+
+    def __init__(self, sz):
+        self.root = None
+        self.size = sz-1
+        self.massiv = [None]*self.size
+
+    def add_branch(self, val):
+        if self.root is None:
+            self.root = val
+            self.massiv.insert(0, self.root)
+        y = self.poisk(self.root, val)
+        if y is None:
+            return None
+        elif y > 0:
+            return self.massiv[y]
+        elif y < 0:
+            self.massiv[-y] = val
+            node = val
+            node.parent = self.massiv[(-y-1)//2]
+            node.level = node.parent.level + 1
+            node = node.parent
+            if node.value > val.value:
+                node.l = val
+                node = node.l
+            if node.value < val.value:
+                node.r = val
+                node = node.r
+
+    def poisk(self, x, val):
+        if val is None:
+            return None
+        node = x
+        if node is not None and node.value == val.value:
+            return self.massiv.index(node)
+        else:
+            if val.value > node.value:
+                if node.r is None and (2*(self.massiv.index(node))+2) <= self.size:
+                    return -(2*(self.massiv.index(node))+2)
+                elif (2*(self.massiv.index(node))+2) > self.size:
+                    return None
+                x = self.poisk(node.r, val)
+                return x
+            elif val.value < node.value:
+                if node.l is None and (2*(self.massiv.index(node))+1) <= self.size:
+                    return -(2*(self.massiv.index(node))+1)
+                elif (2*(self.massiv.index(node))+1) > self.size:
+                    return None
+                x = self.poisk(node.l, val)
+                return x
+            else:
+                return None
+
+    def printall(self):
+        z = 0
+        for i in range(self.size):
+            if self.massiv[i] is not None:
+                z += 1
+                print(self.massiv[i].value, "level", self.massiv[i].level)
+
+        print("razmer = ", z)
+
+class TestMethods(unittest.TestCase):
+
+    def initialize(self):
+        self.s = ArrayTree(7)
+        self.s.add_branch(Node(12))
+        self.s.add_branch(Node(16))
+        self.s.add_branch(Node(14))
+        self.s.add_branch(Node(8))
+
+    def test_poisk(self):
+        self.initialize()
+        self.s.add_branch(Node(20))
+        self.s.add_branch(Node(10))
+        self.assertTrue(self.s.poisk(self.s.root, Node(6)) == -3)       # Найдено свободное место
+        self.s.add_branch(Node(6))
+        self.assertTrue(self.s.poisk(self.s.root, Node(3)) is None)     # Все дерево пройдено,совпадений нет
+        self.assertTrue(self.s.poisk(self.s.root, Node(20)) == 6)       # Присутствующее значение
+        self.s = None
+
+    def test_add(self):
+        self.initialize()
+        self.assertTrue(self.s.poisk(self.s.root, Node(20)) == -6)      # Место под значение свободно
+        self.s.add_branch(Node(20))                                     # Значение добавлено
+        self.assertTrue(self.s.poisk(self.s.root, Node(20)) == 6)       # Место под значение занято
+        self.s.add_branch(Node(6))
+        self.s.add_branch(Node(10))
+        self.assertTrue(self.s.massiv[6].value == 20)                   # Соответствие значений индексам в массиве
+        self.assertTrue(self.s.massiv[5].value == 14)
+        self.assertTrue(self.s.massiv[4].value == 10)
+        self.assertTrue(self.s.massiv[3].value == 6)
+        self.assertTrue(self.s.massiv[2].value == 16)
+        self.assertTrue(self.s.massiv[1].value == 8)
+        self.assertTrue(self.s.massiv[0].value == 12)
+        self.s = None
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ rArrayTree/rArrayTree.py
@@ -0,0 +1,156 @@
+import arraytree
+import random
+
+spisok = []
+for i in range(10):
+    spisok.append(random.randint(0,3))
+
+spisok = sorted(spisok)
+spisok2 = []
+
+for i in range(10):
+    spisok2.append(arraytree.Node(spisok[i]))
+
+
+def masiv(spisok, sz):
+    mtree = [None]*sz
+    l = len(spisok)//2
+    mtree[0] = spisok.pop(l)
+    spisok1 = spisok[0:l]
+    spisok2 = spisok[l:len(spisok)]
+    mtree.insert(2 * (mtree.index(mtree[0])) + 1, spisok1.pop(l // 2))
+    mtree.insert(2 * (mtree.index(mtree[0])) + 2, spisok2.pop(l // 2))
+    node = mtree[0]
+    node.l = mtree[2 * (mtree.index(mtree[0])) + 1]
+    node.r = mtree[2 * (mtree.index(mtree[0])) + 2]
+    povtor(spisok1, mtree, node.l)
+    povtor(spisok2, mtree, node.r)
+    return mtree
+
+
+def povtor(spisok, mtree, parent):
+    print("na4alo,spisok = ",len(spisok),"koren", parent.value)
+    l = (len(spisok) // 2)
+    x1, x2 = None, None
+    if len(spisok) == 1:
+        spisok1, spisok2 = [], []
+        if spisok[0].value < parent.value:
+            x1 = spisok[0]
+        elif spisok[0].value > parent.value:
+            x2 = spisok[0]
+    else:
+        spisok1 = spisok[0:l]
+        if len(spisok1) > 0:
+            if spisok1[l//2] is not None and parent is not None:
+                if spisok1[l//2].value < parent.value:
+                    x1 = spisok1.pop(l//2)
+        else:
+            x1 = None
+
+        spisok2 = spisok[l:len(spisok)]
+        if len(spisok2) > 0:
+            if spisok2[l//2] is not None and parent is not None:
+                if spisok2[l//2].value > parent.value:
+                    x2 = spisok2.pop(l//2)
+                else:
+                    x2 = None
+        else:
+            x2 = None
+    print()
+    print("выбраны иксы", end=" ")
+    if x1 is not None:
+        print("x1", x1.value, end=" ")
+    else:
+        print("x1", x1, end=" ")
+    if x2 is not None:
+        print("x2", x2.value, end=" ")
+    else:
+        print("x2", x2, end=" ")
+    print()
+    print()
+
+    if mtree[2 * (mtree.index(parent)) + 1] is None and x1 is not None:
+        print("mesto", [2 * (mtree.index(parent)) + 1], "svobodbo")
+        mtree[2 * (mtree.index(parent)) + 1] = x1
+        parent.l = mtree[2 * (mtree.index(parent)) + 1]
+        print("mesto", [2 * (mtree.index(parent)) + 1], "zan9to", x1.value, "vlevo k koren", parent.value)
+        spisok.remove(x1)
+        if len(spisok1) > 0:
+            povtor(spisok1, mtree, parent.l)
+    elif x1 is None and len(spisok1) > 0:
+        povtor(spisok1, mtree, parent)
+    elif mtree[2 * (mtree.index(parent)) + 1] is not None and x1 is not None:
+        spisok1.insert(len(spisok1) // 2, x1)
+        povtor(spisok1, mtree, mtree[2 * (mtree.index(parent)) + 1])
+
+    if mtree[2 * (mtree.index(parent)) + 2] is None and x2 is not None:
+        print("mesto", [2 * (mtree.index(parent)) + 2], "svobodbo")
+        mtree[2 * (mtree.index(parent)) + 2] = x2
+        parent.r = mtree[2 * (mtree.index(parent)) + 2]
+        print("mesto", [2 * (mtree.index(parent)) + 2], "zan9to", x2.value, "vpravo k koren", parent.value)
+        spisok.remove(x2)
+        if len(spisok2) > 0:
+            povtor(spisok2, mtree, parent.r)
+    elif x2 is None and len(spisok2) > 0:
+        povtor(spisok2, mtree, parent)
+    elif mtree[2 * (mtree.index(parent)) + 2] is not None and x2 is not None:
+        spisok2.insert(len(spisok2)//2, x2)
+        povtor(spisok2, mtree, mtree[2 * (mtree.index(parent)) + 2])
+
+    print("konec", len(spisok), parent.value)
+    return None
+
+
+
+def vderevo(items):
+
+    tree = arraytree.ArrayTree(10000)            # Добавление корневого элемента
+    tree.add_branch(items.pop(len(items)//2))
+
+    ostatok = 0                                         # Список становится четным перед делением на 2 части
+    l = None                                            # Лишний элемент, при наличии, сохраняется
+    if len(items) % 2 == 0:
+        l = len(items)//2
+    elif len(items) % 2 == 1:
+        ostatok = items.pop()
+        l = len(items)//2
+
+    items1 = items[0:l]                                 # Список делится на 2 части
+    items2 = items[l:]
+
+    for i in range(l):                                  # Каждый элемент каждой части из середины добавляется в дерево
+        tree.add_branch(items1[len(items1)//2])         # Проверяется размер, доподлняется новыми ячейчками,если нужно
+        #razmer(tree, items1[len(items1)//2])            # Элемент удаляется из списка
+        items1.pop(len(items1)//2)
+    for i in range(l):
+        tree.add_branch(items2[len(items2)//2])
+        #razmer(tree, items2[len(items2) // 2])
+        items2.pop(len(items2)//2)
+
+    if ostatok != 0:                                    # Остаток, при наличии, добавляется в дерево
+        tree.add_branch(ostatok)
+    return tree
+
+
+def razmer(tree, item):
+    if tree.poisk(tree.root, item) is None:
+        return None
+    x = tree.massiv[tree.poisk(tree.root, item)]
+    if x is not None and tree.size < ((2 ** (x.level + 1)) - 1):
+        l1 = ((2 ** (x.level + 1)) - 1) - tree.size
+        for j in range(l1):
+            tree.massiv.append(None)
+        tree.size = ((2 ** (x.level + 1)) - 1)
+    print(x.level)
+    return tree
+
+
+s = masiv(spisok2, 30000)
+print("_____")
+z = 0
+for i in range(len(s)):
+    if s[i] is not None:
+        print(s[i].value, i)
+        z += 1
+print(z)
+#rint(s[0].r.l.l.value)
