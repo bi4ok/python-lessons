f440f453e69f19ede4c8c4ccf6fcd079cca3ea86
bi4ok <bi4ok23@gmail.com>
2018-11-26 18:39:21 +0300

    сортировка Шелла

--- sortirovka/sort(baza).py
+++ sortirovka/sort(baza).py
@@ -5,7 +5,7 @@ s1 = []
 for i in range(100):
     s1.append(random.randint(0, 100))
 print(s1)
-s2 = [7, 6, 5, 4, 3, 2, 1]
+s2 = [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
 
 def sort1(s, l):
@@ -29,8 +29,8 @@ def sort2(s, l):
         i += 1
 
 
-print(timeit.Timer(lambda: sort1(s1, 1)).timeit(number=100))
-print(timeit.Timer(lambda: sort2(s1, 1)).timeit(number=100))
+print(timeit.Timer(lambda: sort1(s2, 1)).timeit(number=100))
+print(timeit.Timer(lambda: sort2(s2, 1)).timeit(number=100))
 
 sort1(s2, 3)
 print(s2)
