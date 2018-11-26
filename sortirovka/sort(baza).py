0b3f8eb1638ad4d28cb8b98af631f1021aed42ab
bi4ok <bi4ok23@gmail.com>
2018-11-26 10:46:12 +0300

    sort(baza)

---
+++ sortirovka/sort(baza).py
@@ -0,0 +1,36 @@
+import timeit
+import random
+
+s1 = []
+for i in range(100):
+    s1.append(random.randint(0, 100))
+print(s1)
+s2 = [7, 6, 5, 4, 3, 2, 1]
+
+
+def sort1(s, l):
+    i = 0
+    while i + l < len(s):
+        for j in range(i, len(s), l):
+            for k in range(i, j, l):
+                if s[j] < s[k]:
+                    s.insert(k, s.pop(j))
+                    s.insert(j, s.pop(k+1))
+        i += 1
+
+
+def sort2(s, l):
+    i = 0
+    while i + l < len(s):
+        for j in range(i, len(s), l):
+            for k in range(i, j, l):
+                if s[j] < s[k]:
+                    s[j], s[k] = s[k], s[j]
+        i += 1
+
+
+print(timeit.Timer(lambda: sort1(s1, 1)).timeit(number=100))
+print(timeit.Timer(lambda: sort2(s1, 1)).timeit(number=100))
+
+sort1(s2, 3)
+print(s2)
