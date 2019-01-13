881dc4b44dd5d185541a2a48cd94644f3c5524c2
bi4ok <bi4ok23@gmail.com>
2019-01-13 20:50:06 +0300

    Update 9.py

--- HashTable/9.py
+++ HashTable/9.py
@@ -6,6 +6,8 @@ class HashTable:
 
     def hash_fun(self, val):
         index = 0
+        if self.slots[index] is None:
+            return index
         val = str(val)
         for i in range(len(val)):
             if int(val[i]) != 0:
@@ -21,22 +23,20 @@ class HashTable:
             if self.slots[index] is None:
                 return index
             else:
-                if index + self.step <= (self.size - 1):
-                    index += self.step
-                    if self.slots[index] is None:
-                        return index
-                elif index + self.step > self.size-1:
-                    index = 0 + (index + self.step - self.size)
-                    if self.slots[index] is None:
-                        return index
+                index += self.step
+                while index > self.size - 1:
+                    index -= (self.size - 1)
+                if self.slots[index] == value:
+                    return index
         return None
 
     def put(self, value):
         x = self.seek_slot(value)
         if x is not None:
-            self.slots[x] = int(value)
+            self.slots[x] = value
             return x
         else:
+            print("_______________________________________________________")
             return None
 
     def find(self, value):
@@ -49,8 +49,11 @@ class HashTable:
                     index += self.step
                     if self.slots[index] == value:
                         return index
-                elif index + self.step > self.size - 1:
-                    index = 0 + (index + self.step - self.size)
+                else:
+                    x = self.step
+                    while x > (self.size - 1):
+                        x -= (self.size - 1)
+                    index = 0 + (index + x - self.size)
                     if self.slots[index] == value:
                         return index
         return None
