c03798f949f6fe71c2849f1223ba35f46b467122
bi4ok <bi4ok23@gmail.com>
2019-01-21 14:13:19 +0300

    Update concurrency.py

--- concurrency/concurrency.py
+++ concurrency/concurrency.py
@@ -9,7 +9,7 @@ def odc4et(pt, sp):
         sum = 0
         for x in sp4:
             sum += x
-        time.sleep(0.05)
+            time.sleep(0.05)
         res[id] = sum
 
     n = len(sp)/pt
@@ -17,15 +17,17 @@ def odc4et(pt, sp):
     for i in range(pt):
         x = Thread(target=s4et, name='Thread N'+str(i), args=('Id'+str(i), sp[int(n*i):int(n*(i+1))], results))
         x.start()
-    x.join()
-
+    while x.is_alive() is True:
+        time.sleep(0.5)
     itog = 0
-    for l in results:
-        itog += results[l]
-    print(itog)
+    for i in results:
+        itog += results[i]
+    return itog
+
 
 a = []
-for i in range(100000):
+for i in range(10000):
     a.append(random.randint(0, 1000))
 print(sum(a))
-odc4et(10,a)
+s = odc4et(10, a)
+print(s)
