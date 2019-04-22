518b2d3365e1ad9195415f5bea56a59b4e7fdf51
bi4ok <bi4ok23@gmail.com>
2019-04-22 12:11:45 +0300

    Update bihastree.py

--- bitrees/bihastree.py
+++ bitrees/bihastree.py
@@ -109,3 +109,47 @@ class BST:
             if i is not None:
                     z += 1
         return z
+
+    def WideAllNodes(self, l=0, spisok=[]):
+        if l == 0:
+            spisok = [self.Root]
+        s4et = l
+        for i in spisok[s4et:]:
+            if i.LeftChild is not None:
+                spisok.append(i.LeftChild)
+            if i.RightChild is not None:
+                spisok.append(i.RightChild)
+            s4et += 1
+        if s4et != len(spisok):
+            self.WideAllNodes(l=s4et, spisok=spisok)
+        return spisok
+
+    def DeepAllNodes(self, order, node=0, spisok=[]):
+        if node == 0:
+            node = self.Root
+
+        def levo():
+            if node.LeftChild is not None:
+                self.DeepAllNodes(order, node.LeftChild, spisok)
+
+        def centr():
+            if node not in spisok:
+                spisok.append(node)
+
+        def pravo():
+            if node.RightChild is not None:
+                self.DeepAllNodes(order, node.RightChild, spisok)
+
+        deistviya = [levo, centr, pravo]
+        if order == 0:
+            for i in deistviya:
+                i()
+        elif order == 1:
+            deistviya[0]()
+            deistviya[2]()
+            deistviya[1]()
+        elif order == 2:
+            deistviya[1]()
+            deistviya[0]()
+            deistviya[2]()
+        return spisok
