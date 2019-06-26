a1869b77643f69bde4585f72e87f1818b291ca47
bi4ok <bi4ok23@gmail.com>
2019-06-26 13:52:55 +0300

    Create balans_massiv_for_tree.py

---
+++ bitrees/balans_massiv_for_tree.py
@@ -0,0 +1,15 @@
+def GenerateBBSTArray(a):
+    balans_massiv = []
+    a = sorted(a)
+
+    def balansirovka(a, bm):
+        if not a:
+            return None
+        centr = int(len(a)/2)
+        bm.append(a[centr])
+        balansirovka(a[:centr], bm)
+        balansirovka(a[centr+1:], bm)
+        return bm
+
+    balans_massiv = balansirovka(a, balans_massiv)
+    return balans_massiv
