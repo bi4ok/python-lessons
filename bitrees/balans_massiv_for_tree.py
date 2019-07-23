350d389e5d5408f356add481c404574d358f21f0
bi4ok <bi4ok23@gmail.com>
2019-07-23 16:48:43 +0300

    massiv for tree

--- bitrees/balans_massiv_for_tree.py
+++ bitrees/balans_massiv_for_tree.py
@@ -1,15 +1,22 @@
 def GenerateBBSTArray(a):
-    balans_massiv = []
+    balans_massiv = [None] * 3
     a = sorted(a)
+    global u
+    u = 1
 
-    def balansirovka(a, bm):
+    def balansirovka(a, bm, x):
         if not a:
             return None
+        if x >= len(bm):
+            global u
+            u += 1
+            l1 = 2 ** (u + 1) - 1
+            bm += ([None] * (l1 - len(bm)))
         centr = int(len(a)/2)
-        bm.append(a[centr])
-        balansirovka(a[:centr], bm)
-        balansirovka(a[centr+1:], bm)
+        bm[x] = a[centr]
+        balansirovka(a[:centr], bm, x * 2 + 1)
+        balansirovka(a[centr+1:], bm, x * 2 + 2)
         return bm
 
-    balans_massiv = balansirovka(a, balans_massiv)
+    balans_massiv = balansirovka(a, balans_massiv, 0)
     return balans_massiv
