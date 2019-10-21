c2b3f1ed946546125cf0da071df14a7ddb3cce93
bi4ok <bi4ok23@gmail.com>
2019-10-21 18:21:53 +0300

    graph v2.0

--- data structure/graph/graph.py
+++ data structure/graph/graph.py
@@ -20,20 +20,21 @@ class SimpleGraph:
             pass
 
     def AddEdge(self, v1, v2):
-        if v1 and v2 in range(len(self.vertex)):
+        if max(v1, v2) in range(len(self.vertex)):
+            print(max(v1, v2))
             self.m_adjacency[v1][v2] = 1
             self.m_adjacency[v2][v1] = 1
         else:
             pass
 
     def IsEdge(self, v1, v2):
-        if v1 and v2 in range(len(self.vertex)):
+        if max(v1, v2) in range(len(self.vertex)):
             if self.m_adjacency[v1][v2] == 1:
                 return True
         return False
 
     def RemoveEdge(self, v1, v2):
-        if v1 and v2 in range(len(self.vertex)):
+        if max(v1, v2) in range(len(self.vertex)):
             if self.m_adjacency[v1][v2] == 1:
                 self.m_adjacency[v1][v2] = 0
                 self.m_adjacency[v2][v1] = 0
